2f8dae6e19e7833d2ab0cc3d6824efa031334530
avinash Shah <avinash08180@gmail.com>
2026-06-21 01:57:54 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: "avinashboy.aternos.me",
+  port: 47974,
+  username: exploser"
+})
+
+bot.on('spawn', () => {
+  console.log("Bot online")
+  setInterval(() => {
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 1000)
+  }, 30000)
+})
